c638d506ca3b1b12686d647dd48ed575624b808c
Canberk <*canberk.alkan91@gmail.com*>
2021-01-19 23:43:37 +0100

    Produktseite überarbeitet
    Karten erstellt, indem Produktbilder, der Produktname sowie eine Beschreibung hinzugefügt werden kann.

--- webproject/src/Seiten/Produkte.jsx
+++ webproject/src/Seiten/Produkte.jsx
@@ -1,23 +1,104 @@
-import React from 'react';
-import '../App.css';
-import './Produkte.css'
+import React, { Component } from "react";
+import "../App.css";
+import "./Produkte.css";
 
-export default function Produkte() {
-  return (
-    <div className="list">
-      <h1>test</h1>
-      <div className="grid">
-        <div className="box box1"></div>
-        <div className="box box2"></div>
-        <div className="box box3"></div>
-        <div className="box box4"></div>
-        <div className="box box5"></div>
-        <div className="box box6"></div>
-        <div className="box box7"></div>
-        <div className="box box8"></div>
-        <div className="box box9"></div>
-        <div className="box box10"></div>
+export default class Produkte extends Component {
+  constructor() {
+    super();
+    this.state = {
+      box: [
+        {
+          image:
+            "",
+          name: "Kaffee Orginal",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "Espresso",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "Cappucino",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "test",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "test",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "test",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "test",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "test",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "test",
+          description: "test",
+        },
+        {
+          image:
+            "",
+          name: "test",
+          description: "test",
+        },
+      ],
+    };
+  }
+  render() {
+
+    return (
+      <div className="card-grid">
+        {this.state.box.map((item) => {
+          return (
+            <div className="card-box">
+              <img className="card-image" src={item.image} />
+              <div className="card-content">
+                <h3 className="card-name">{item.name}</h3>
+                <p className="card-description">{item.description}</p>
+                <div class="card-stars">
+                  <div>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                  </div>
+                  <div className="card-rating">Bewertungen</div>
+                </div>
+                <button className="card-button">
+                  <div className="fas fa-shopping-cart"></div>
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </div>
-  )
+    );
+  }
 }
