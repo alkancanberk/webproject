52a09b66a28465b6cfea551365c8060b3fea7046
Canberk <*canberk.alkan91@gmail.com*>
2021-01-29 18:57:14 +0100

    Checkout ausgebaut
    Zum Checkout wurde eine Form hinzugefügt.

--- webproject/src/Seiten/Checkout.jsx
+++ webproject/src/Seiten/Checkout.jsx
@@ -1,11 +1,84 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import './Checkout.css'
 
 export default class Checkout extends Component {
-  
-    render() {
-        return (
-            <h1>Test</h1>
-        )
-    }
-}
+  state = {
+    Vorname: "",
+    Nachname: " ",
+    Ort: " ",
+    StraßeHausnummer: " ",
+    Email: " ",
+  };
 
+  //Durch diese Funktion wird der state mit der aktuellen Eingabe des Benutzers für zum Beispiel den Vornamen aktualisiert
+  replace = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <form className="grid-box">
+        <div>
+          <div>
+            <label className="label">Vorname</label>
+            <input
+              className="grid-layer"
+              name="Vorname"
+              placeholder="Vorname"
+              value={this.state.Vorname}
+              onChange={(event) => this.replace(event)}
+            />
+          </div>
+          <div>
+            <label className="label">Nachname</label>
+            <input
+              className="grid-layer"
+              name="Nachname"
+              placeholder="Nachname"
+              value={this.state.Nachname}
+              onChange={(event) => this.replace(event)}
+            />
+          </div>
+          <div>
+            <label className="label">Ort</label>
+            <input
+              className="grid-layer"
+              name="Ort"
+              placeholder="Ort"
+              value={this.state.Ort}
+              onChange={(event) => this.replace(event)}
+            />
+          </div>
+          <div>
+            <label className="label">Straße und Hausnummer</label>
+            <input
+              className="grid-layer1"
+              name="StraßeHausnummer"
+              placeholder="Straße und Hausnummer"
+              value={this.state.StraßeHausnummer}
+              onChange={(event) => this.replace(event)}
+            />
+          </div>
+          <div>
+            <label className="label">Email</label>
+            <input
+              className="grid-layer1"
+              name="Email"
+              placeholder="E-Mail"
+              value={this.state.Mail}
+              onChange={(event) => this.replace(event)}
+            />
+            <div>
+              <Link to="/home">
+                <button>Bestätigen</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
